6a29dd8727e97c574b89883ec65afc92ef724dc4
freedomwait <fengweifree@gmail.com>
2017-09-26 12:08:56 +0800

    init demo page with react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img className="App-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1506407203182&di=614e75ffc93a1c2260c5fe845fa0f882&imgtype=0&src=http%3A%2F%2Fimage12.m1905.cn%2Fmapps%2Fuploadfile%2Fedu%2F2014%2F1225%2Fthumb_0_400_600_2014122511060228826.jpg" alt="logo" />
+        </div>
+        <div className="App-intro">
+          "I guess it comes down to a simple choice:get busy living or get busy dying."
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
